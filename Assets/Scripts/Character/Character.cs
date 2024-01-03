1e75d82ae4dad5bea011ff986c615b4c0cceba85
raonrabbit <raonrabbit@gmail.com>
2024-01-04 02:21:23 +0900

    feat: 캐릭터 추상클래스 구현

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    protected bool _isDead = false;
+
+    public abstract void Move();
+    public abstract void Attack();
+    public void Die(){
+        if(!_isDead){
+            _isDead = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
