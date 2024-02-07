ced15a4deccfbaf8233316e0479edf07d26ad738
raonrabbit <raonrabbit@gmail.com>
2024-02-07 10:21:22 +0900

    add: 네트워크매니저 생성

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    
+}
