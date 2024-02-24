c625dde6d950af90418d491534179fbc5f7db778
raonrabbit <raonrabbit@gmail.com>
2024-02-25 03:47:22 +0900

    rename: HealthBar파일을 PlayerUI로 변경 / Player의 이름, 체력바 등을 관리하는 파일로 변경

--- Assets/Scripts/Character/PlayerUI.cs
+++ Assets/Scripts/Character/PlayerUI.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private Slider hpBar;
+    [SerializeField] private Text nickName;
     [SerializeField] private Character character;
     [SerializeField] private Vector3 offset;
     void Update(){
         hpBar.value = character.GetHp;
         hpBar.transform.rotation = Quaternion.identity;
         hpBar.transform.position = character.transform.position + offset;
+        nickName.transform.rotation = Quaternion.identity;
+        nickName.transform.position = character.transform.position + offset - new Vector3(0, 0.3f, 0);
     }
 }
