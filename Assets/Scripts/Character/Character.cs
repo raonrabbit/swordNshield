fc7b457d00e0467d81ee4d8b0d26533605c1e7e5
raonrabbit <raonrabbit@gmail.com>
2024-02-26 17:26:32 +0900

    feat: Character를 Photon으로 position, rotation 동기화

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
-public abstract class Character : MonoBehaviour
+public abstract class Character : MonoBehaviourPunCallbacks, IPunObservable
 {
     public Sword sword;
     protected Animator animator;
+    protected PhotonView _photonView;
+    protected Rigidbody2D _rigidBody2D;
     protected bool _isDead = false;
+    [SerializeField] protected Text _nickName;
 
     //기본 설정
     [SerializeField] protected float _speed = 2f;
@@ -27,15 +34,46 @@ public abstract class Character : MonoBehaviour
     [SerializeField] protected bool _isDefending = false;
     protected bool _canDefend = true;
 
+    //동기화
+    protected Vector3 _currentPosition;
+    protected Quaternion _currentRotation;
+
+    protected void Awake(){
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        _photonView = GetComponent<PhotonView>();
+        _nickName.text = _photonView.IsMine ? PhotonNetwork.NickName : _photonView.Owner.NickName;
+        _nickName.color = _photonView.IsMine ? Color.green : Color.red;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo message){
+        if(stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(_currentHp);
+        }
+        else
+        {
+            _currentPosition = (Vector3)stream.ReceiveNext();
+            _currentRotation = (Quaternion)stream.ReceiveNext();
+            _currentHp = (int)stream.ReceiveNext();
+        }
+    }
+
     public int GetHp{
         get => _currentHp;
     }
     public float AttackTime{
         get => _attackTime;
     }
+    public Text GetNickName{
+        get => _nickName;
+    }
     public abstract void Move();
-    public void GetDamage(Character other){
-        if(!(_isDefending && FaceToOther(other))){
+
+    [PunRPC]
+    public void GetDamage(Vector3 attackerPosition){
+        if(!(_isDefending && FaceToOther(attackerPosition))){
             _currentHp -= _damage;
             if(_currentHp <= 0) Die();
         }
@@ -74,8 +112,8 @@ public abstract class Character : MonoBehaviour
         _canDefend = true;
     }
     
-    public bool FaceToOther(Character other){
-        Vector2 directionToOther = (other.transform.position - transform.position).normalized;
+    public bool FaceToOther(Vector3 otherPosition){
+        Vector2 directionToOther = (otherPosition - transform.position).normalized;
         if(Vector2.Angle(transform.up, directionToOther) < 40f){
             return true;
         }
