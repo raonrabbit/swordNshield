5771578cc59cf5be2b0a4143f1b5478d54332857
raonrabbit <raonrabbit@gmail.com>
2024-03-02 04:34:00 +0900

    feat: CoolTime을 화면에 표시하기 위한 CoolTimeUI 생성 및 이미지 연결

---
+++ Assets/Scripts/Character/CoolTimeUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolTimeUI : MonoBehaviour
+{
+    private static CoolTimeUI instance = null;
+    public Image DashSkillImage;
+    public Image ShieldSkillImage;
+    public Image AttackSkillImage;
+
+    void Awake(){
+        if(instance == null) instance = this;
+        else Destroy(this.gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static CoolTimeUI Instance{
+        get => instance;
+    }
+}
