46104b86183a94be930de65673a113cd5b509119
raonrabbit <raonrabbit@gmail.com>
2024-03-07 01:03:35 +0900

    feat: KillScore를 증가시키고 관리할 KillScoreManager 구현

---
+++ Assets/Scripts/Manager/KillScoreManager.cs
@@ -0,0 +1,25 @@
+using Photon.Pun;
+
+public class KillScoreManager : MonoBehaviourPunCallbacks
+{
+    private static KillScoreManager instance = null;
+    private ExitGames.Client.Photon.Hashtable properties;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this.gameObject);
+        properties = new ExitGames.Client.Photon.Hashtable { { "Score", 0 } };
+        PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
+    }
+    
+    public static KillScoreManager Instance => instance;
+
+    [PunRPC]
+    public void KillPlayer(Photon.Realtime.Player target)
+    {
+        var currentScore = (int)target.CustomProperties["Score"];
+        currentScore++;
+        target.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "Score", currentScore } });
+    }
+}
