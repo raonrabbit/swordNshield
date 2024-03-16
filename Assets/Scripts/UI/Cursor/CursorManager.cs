7e834bb8c469dcb5e36928a8d110e04deb0e4230
raonrabbit <raonrabbit@gmail.com>
2024-03-17 02:17:30 +0900

    feat: 커서의 모양을 바꾸는 함수 작성(커서매니저)

---
+++ Assets/Scripts/UI/Cursor/CursorManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    private static CursorManager instance = null;
+    public Texture2D defaultCursor;
+    public Texture2D attackCursor;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this.gameObject);
+        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
+    }
+    
+    public static CursorManager Instance => instance;
+
+    public void SetDefaultCursor()
+    {
+        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void SetAttackCursor()
+    {
+        Debug.Log("Change to attack");
+        Cursor.SetCursor(attackCursor, Vector2.zero, CursorMode.Auto);
+    }
+}
