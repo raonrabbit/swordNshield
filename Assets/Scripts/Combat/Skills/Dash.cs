646f81ae763362bb69f23fd8058b3168e402f838
raonrabbit <raonrabbit@gmail.com>
2024-03-22 23:40:35 +0900

    refactor: 기존의 Dash를 리팩토링한 다른 코드에 호환되도록 변경

---
+++ Assets/Scripts/Combat/Skills/Dash.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+namespace SwordNShield.Combat.Skills
+{
+    public class Dash : MonoBehaviour, ISkill
+    {
+        public Controller.PlayerController Owner { get; set; }
+        [SerializeField] private Sprite skillImage;
+        [SerializeField] private KeyCode keyCode = KeyCode.F;
+        [SerializeField] private float coolTime = 3f;
+        [SerializeField] private float actionTime = 0.5f;
+        [SerializeField] private float dashSpeed = 10f;
+        private bool canExecute = true;
+        private bool isDashing;
+        private Vector2 dashDirection;
+        private Rigidbody2D rigidbody2D;
+
+        private void Awake()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        public KeyCode GetKeyCode
+        {
+            get => keyCode;
+            set => keyCode = value;
+        }
+
+        public Sprite SkillImage => skillImage;
+        public bool CanExecute => canExecute;
+        public bool IsPlaying => isDashing;
+        public float CoolTime => coolTime;
+        public float ActionTime => actionTime;
+
+        //Dash 실행
+        public void Play()
+        {
+            StartCoroutine(Execute());
+        }
+        
+        public IEnumerator Execute()
+        {
+            if (!canExecute) yield break;
+            Owner.GetMover.CanMove = false;
+            dashDirection = (Owner.GetMouseRay() - (Vector2)transform.position).normalized;
+            float startTime = Time.time;
+            canExecute = false;
+            isDashing = true;
+            while(Time.time - startTime < actionTime){
+                Owner.GetActionScheduler.CancelCurrentAction();
+                rigidbody2D.velocity = dashDirection * dashSpeed;
+                yield return null;
+            }
+            rigidbody2D.velocity = Vector2.zero;
+            Owner.GetMover.CanMove = true;
+            isDashing = false;
+            yield return new WaitForSeconds(coolTime);
+            canExecute = true;
+        }
+    }
+}
