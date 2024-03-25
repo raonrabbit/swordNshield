a8780fd664b2952f3b1f0535463aa65aef9ceb4d
raonrabbit <raonrabbit@gmail.com>
2024-03-26 05:03:16 +0900

    feat: ShieldAttack 스킬 시전시 받는 피해 모두 무시 효과 구현

--- Assets/Scripts/Combat/Skills/Warrior/ShieldAttack.cs
+++ Assets/Scripts/Combat/Skills/Warrior/ShieldAttack.cs
@@ -1,21 +1,38 @@
 using System;
+using System.Collections;
+using System.Numerics;
+using Photon.Pun;
+using SwordNShield.Attributes;
 using SwordNShield.Combat;
 using SwordNShield.Controller;
+using SwordNShield.Function;
+using SwordNShield.Movement;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class ShieldAttack : MonoBehaviour, ISkill
 {
     public PlayerController Owner { get; set; }
     public event EventHandler PlaySkill;
+    [SerializeField] private AnimationClip animationClip;
     [SerializeField] private Sprite skillImage;
     [SerializeField] private KeyCode keyCode;
     [SerializeField] private float coolTime;
     [SerializeField] private float actionTime;
 
+    private AnimationScheduler animationScheduler;
+    private Rotater rotater;
+    private Health health;
     private bool canExecute = true;
     private bool isPlaying;
     private ISkill _skillImplementation;
 
+    void Awake()
+    {
+        animationScheduler = GetComponent<AnimationScheduler>();
+        health = GetComponent<Health>();
+        rotater = GetComponent<Rotater>();
+    }
     public KeyCode GetKeyCode
     {
         get => keyCode;
@@ -32,6 +49,22 @@ public class ShieldAttack : MonoBehaviour, ISkill
     {
         if (!canExecute) return;
         PlaySkill!.Invoke(this, EventArgs.Empty);
-        
+        StartCoroutine(ExecuteShieldAttack());
+        Owner.photonView.RPC("ExecuteShieldAttack", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public IEnumerator ExecuteShieldAttack()
+    {
+        rotater.CanRotate = false;
+        animationScheduler.StartAnimation(animationClip);
+        canExecute = false;
+        isPlaying = true;
+        health.CanGetDamage = false;
+        yield return new WaitForSeconds(actionTime);
+        health.CanGetDamage = true;
+        rotater.CanRotate = true;
+        yield return new WaitForSeconds(coolTime);
+        canExecute = true;
     }
 }
