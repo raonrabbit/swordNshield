c4436f0b0f87621049167d1de3352c762ca2b031
raonrabbit <raonrabbit@gmail.com>
2024-03-25 14:27:39 +0900

    rename: 파일 이동

---
+++ Assets/Scripts/UI/CoolTimeUI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwordNShield.UI
+{
+    public class CoolTimeUI : MonoBehaviour
+    {
+        private static CoolTimeUI instance = null;
+        public Image QSkillImage;
+        public Image WSkillImage;
+        public Image ESkillImage;
+        public Image DSkillImage;
+        public Image FSkillImage;
+
+        void Awake(){
+            if(instance == null) instance = this;
+            else Destroy(this.gameObject);
+        }
+
+        public static CoolTimeUI Instance{
+            get => instance;
+        }
+    }
+}
