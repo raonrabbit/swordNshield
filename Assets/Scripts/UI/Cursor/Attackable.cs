c47e21ae7ae1649416a28c0f7f16b6424cd02a6e
raonrabbit <raonrabbit@gmail.com>
2024-03-25 23:41:22 +0900

    refactor: SwordNShield.UI Namespace로 설정

--- Assets/Scripts/UI/Cursor/Attackable.cs
+++ Assets/Scripts/UI/Cursor/Attackable.cs
@@ -2,57 +2,62 @@ using Photon.Pun;
 using UnityEngine;
 using System.Collections;
 
-public class Attackable : MonoBehaviour
+namespace SwordNShield.UI
 {
-    [SerializeField] private GameObject outLine;
-    private Vector2 outLineScale;
-    private Vector2 outLineTargetScale;
-    private PhotonView photonView;
+    public class Attackable : MonoBehaviour
+    {
+        [SerializeField] private GameObject outLine;
+        private Vector2 outLineScale;
+        private Vector2 outLineTargetScale;
+        private PhotonView photonView;
 
-    void Awake()
-    {
-        outLine.SetActive(false);
-        outLineScale = outLine.transform.localScale;
-        outLineTargetScale = outLineScale * 1.2f;
-        photonView = GetComponent<PhotonView>();
-    }
-    private void OnMouseOver()
-    {
-        if (photonView == null || !photonView.IsMine)
-        {
-            if (Input.GetMouseButtonDown(1)) StartCoroutine(OutLineBounce());
-            outLine.SetActive(true);
-            CursorManager.Instance.SetAttackCursor();
-        }
-    }
-
-    private void OnMouseExit()
-    {
-        if (photonView == null || !photonView.IsMine)
+        void Awake()
         {
             outLine.SetActive(false);
-            CursorManager.Instance.SetDefaultCursor();
-        }
-    }
-
-    private IEnumerator OutLineBounce()
-    {
-        float time = 0.0f;
-        while (time < 0.1f)
-        {
-            outLine.transform.localScale = Vector2.Lerp(outLineScale, outLineTargetScale, time / (0.1f));
-            time += Time.deltaTime;
-            yield return null;
-        }
-        time = 0.0f;
-        while (time < 0.1f)
-        {
-            outLine.transform.localScale = Vector2.Lerp(outLineTargetScale, outLineScale, time / (0.1f));
-            time += Time.deltaTime;
-            yield return null;
+            outLineScale = outLine.transform.localScale;
+            outLineTargetScale = outLineScale * 1.2f;
+            photonView = GetComponent<PhotonView>();
         }
 
-        outLine.transform.localScale = outLineScale;
+        private void OnMouseOver()
+        {
+            if (photonView == null || !photonView.IsMine)
+            {
+                if (Input.GetMouseButtonDown(1)) StartCoroutine(OutLineBounce());
+                outLine.SetActive(true);
+                CursorManager.Instance.SetCursor(CursorType.Combat);
+            }
+        }
+
+        private void OnMouseExit()
+        {
+            if (photonView == null || !photonView.IsMine)
+            {
+                outLine.SetActive(false);
+                CursorManager.Instance.SetCursor(CursorType.Default);
+            }
+        }
+
+        private IEnumerator OutLineBounce()
+        {
+            float time = 0.0f;
+            while (time < 0.1f)
+            {
+                outLine.transform.localScale = Vector2.Lerp(outLineScale, outLineTargetScale, time / (0.1f));
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            time = 0.0f;
+            while (time < 0.1f)
+            {
+                outLine.transform.localScale = Vector2.Lerp(outLineTargetScale, outLineScale, time / (0.1f));
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            outLine.transform.localScale = outLineScale;
+        }
+
     }
-    
 }
