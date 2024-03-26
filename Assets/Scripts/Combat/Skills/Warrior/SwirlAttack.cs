3cd17a1d2ac391db37350cd45f0cd6f0801ea4c7
raonrabbit <raonrabbit@gmail.com>
2024-03-27 00:53:09 +0900

    feat: Warrior의 회전스킬 구현

---
+++ Assets/Scripts/Combat/Skills/Warrior/SwirlAttack.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using Photon.Pun;
+using SwordNShield.Attributes;
+using SwordNShield.Controller;
+using SwordNShield.Function;
+using SwordNShield.Movement;
+using UnityEngine;
+
+namespace SwordNShield.Combat.Skills
+{
+ public class SwirlAttack : MonoBehaviourPunCallbacks, ISkill
+{
+    public PlayerController Owner { get; set; }
+    public event EventHandler PlaySkill;
+    [SerializeField] private AnimationClip animationClip;
+    [SerializeField] private Sprite skillImage;
+    [SerializeField] private KeyCode keyCode;
+    [SerializeField] private float coolTime;
+    [SerializeField] private float actionTime;
+    [SerializeField] private float timeBetweenDamages;
+    [SerializeField] private float damagePerTime;
+    [SerializeField] private float damageRange;
+    
+    private AnimationScheduler animationScheduler;
+    private Rotater rotater;
+    private Attacker attacker;
+    private bool canExecute;
+    private bool isPlaying;
+
+    void Awake()
+    { 
+        animationScheduler = GetComponent<AnimationScheduler>();
+        rotater = GetComponent<Rotater>();
+        attacker = GetComponent<Attacker>();
+        canExecute = true;
+    }
+
+    public KeyCode GetKeyCode
+    {
+        get => keyCode;
+        set => keyCode = value;
+    }
+    
+    public Sprite SkillSprite => skillImage;
+    public bool CanExecute => canExecute;
+    public bool IsPlaying => isPlaying;
+    public float CoolTime => coolTime;
+    public float ActionTime => actionTime;
+    
+    public void Play()
+    {
+        if (!canExecute) return;
+        PlaySkill!.Invoke(this, EventArgs.Empty);
+        StartCoroutine(ExecuteSwirlAttack());
+        Owner.photonView.RPC("ExecuteSwirlAttack", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public IEnumerator ExecuteSwirlAttack()
+    {
+        rotater.CanRotate = false;
+        attacker.CanAttack = false;
+        animationScheduler.StartAnimation(animationClip);
+        canExecute = false;
+        isPlaying = true;
+        float startTime = Time.time;
+        while (Time.time - startTime <= actionTime)
+        {
+            if(photonView.IsMine) GiveDamage();
+            yield return new WaitForSeconds(timeBetweenDamages);
+        }
+        rotater.CanRotate = true;
+        attacker.CanAttack = true;
+        yield return new WaitForSeconds(coolTime);
+        canExecute = true;
+    }
+
+    public void GiveDamage()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, damageRange);
+        foreach (var hit in hits)
+        {
+            if (hit.gameObject == gameObject) continue;
+            var health = hit.GetComponent<Health>();
+            if (health != null)
+            {
+                health.GetDamage(gameObject, damagePerTime);
+            }
+        }
+    }
+}   
+}
