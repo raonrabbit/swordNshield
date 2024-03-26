664ff1a08efae3470f88c85b9d6cce3125d412e9
raonrabbit <raonrabbit@gmail.com>
2024-03-27 00:53:46 +0900

    feat: 데미지 텍스트 구현

---
+++ Assets/Scripts/UI/DamageText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class DamageText : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    void OnEnable()
+    {
+        Color currentColor = text.color;
+        text.color = new Color(currentColor.r, currentColor.g, currentColor.b, 0.8f);
+        transform.DOMove(transform.position + new Vector3(0f, 0.5f, 0f), 0.5f);
+        text.DOColor(new Color(currentColor.r, currentColor.g, currentColor.b, 0), 0.5f);
+        StartCoroutine(EnableThis());
+    }
+
+    private IEnumerator EnableThis()
+    {
+        yield return new WaitForSeconds(0.5f);
+        DamageTextManager.Instance.ReturnDamageText(gameObject);
+    }
+}
