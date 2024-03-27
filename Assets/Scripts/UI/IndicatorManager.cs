fb5668e547c939fb3a0cea0ada647cb668d95f28
raonrabbit <raonrabbit@gmail.com>
2024-03-28 07:09:45 +0900

    feat: Indicator 활성화, 비활성화 등 관리 스크립트 구현

---
+++ Assets/Scripts/UI/IndicatorManager.cs
@@ -0,0 +1,59 @@
+using SwordNShield.Combat;
+using UnityEngine;
+
+public class IndicatorManager : MonoBehaviour
+{
+    private ISkill currentSkill;
+    private Vector2 mousePosition;
+
+    void Update()
+    {
+        if (currentSkill != null)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Input.GetKeyUp(currentSkill.GetKeyCode))
+            {
+                currentSkill.Indicator.SetActive(false);
+                currentSkill.Play(mousePosition);
+                currentSkill = null;
+            }
+            if (Input.GetMouseButton(1))
+            {
+                currentSkill?.Indicator.SetActive(false);
+                currentSkill = null;
+            }
+            else RotateIndicatorToMousePosition();
+        }
+    }
+
+    public void ActivateIndicator(ISkill skill)
+    {
+        if (!skill.CanExecute) return;
+        if (currentSkill != null)
+        {
+            currentSkill.Indicator.SetActive(false);
+        }
+
+        currentSkill = skill;
+        currentSkill.Indicator.SetActive(false);
+        currentSkill = skill;
+        currentSkill.Indicator.SetActive(true);
+    }
+
+    public void DeActivateIndicator()
+    {
+        if (currentSkill != null)
+        {
+            currentSkill.Indicator.SetActive(false);
+            currentSkill = null;
+        }
+    }
+
+    void RotateIndicatorToMousePosition()
+    {
+        if (currentSkill == null) return;
+        Vector2 direction = mousePosition - (Vector2)transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
