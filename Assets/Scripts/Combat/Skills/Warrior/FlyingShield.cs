278d2ad1a52744f88f2b328948e8e56e8054637b
raonrabbit <raonrabbit@gmail.com>
2024-03-28 07:24:02 +0900

    feat: ShieldAttack에서 발사하는 Shield에 이동 및 데미지부여 기능 구현

---
+++ Assets/Scripts/Combat/Skills/Warrior/FlyingShield.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using Photon.Pun;
+using UnityEngine;
+using SwordNShield.Attributes;
+
+namespace SwordNShield.Combat
+{
+    public class FlyingShield : MonoBehaviourPunCallbacks
+    {
+        private GameObject owner;
+        private float damage;
+        private PhotonView pv;
+        private bool isMine;
+
+        public void Play(GameObject attacker, float distance, float duration, float damage)
+        {
+            owner = attacker;
+            pv = owner.GetComponent<PhotonView>();
+            if (pv != null) isMine = pv.IsMine;
+            this.damage = damage;
+            StartCoroutine(Execute(distance, duration));
+        }
+        
+        IEnumerator Execute(float distance, float duration)
+        {
+            Vector3 start = transform.position;
+            Vector3 end = start + transform.up * distance;
+
+            float currentTime = 0f;
+
+            while (currentTime < duration)
+            {
+                transform.position = Vector3.Lerp(start, end, currentTime / duration);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = end;
+            Destroy(gameObject);
+        }
+
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (isMine) return;
+            if (other.transform == owner.transform) return;
+            Health health = other.GetComponent<Health>();
+            if (health != null)
+            {
+                health.GetDamage(owner, damage);
+            }
+        }
+    }
+}
