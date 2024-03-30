323addf1356f950d9b7956e21bc186bd6687cfa4
raonrabbit <raonrabbit@gmail.com>
2024-03-31 03:38:25 +0900

    feat: 기절시 이펙트 효과 구현

---
+++ Assets/Scripts/UI/State/StunEffect.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace SwordNShield.UI
+{
+    public class StunEffect : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        private Color targetColor;
+        private Color originalColor;
+        private Sequence sequence;
+        void Awake()
+        {
+            originalColor = spriteRenderer.color;
+            targetColor = originalColor;
+            originalColor.a = 0f;
+            targetColor.a = 1f;
+        }
+
+        void OnEnable()
+        {
+            spriteRenderer.color = originalColor;
+
+            sequence = DOTween.Sequence();
+            sequence.Append(transform.DORotate(new Vector3(0, 180, 180), 0.5f).SetEase(Ease.InOutCubic));
+            sequence.Join(DOTween.To(() => spriteRenderer.color, x => spriteRenderer.color = x, targetColor, 0.5f)
+                .SetEase(Ease.InOutCubic));
+        }
+
+        void OnDisable()
+        {
+            sequence?.Kill();
+        }
+    }   
+}
