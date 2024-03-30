018424a8cc5e30981fe2c789cb771e4d99956955
raonrabbit <raonrabbit@gmail.com>
2024-03-31 03:32:17 +0900

    feat: 본인 여부에 따른 HealthBar의 색상 변경

--- Assets/Scripts/UI/HealthBarUI.cs
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -1,20 +1,32 @@
+using Photon.Pun;
 using SwordNShield.Attributes;
 using UnityEngine;
 using UnityEngine.UI;
 using SwordNShield.Movement;
+using SwordNShield.UI;
 
-public class HealthBarUI : MonoBehaviour
+public class HealthBarUI : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Slider hpBar;
     [SerializeField] private Text nickName;
     [SerializeField] private Mover mover;
     [SerializeField] private Vector3 offset;
     [SerializeField] private Health health;
+    private 
+
+    void Awake()
+    {
+        Image fillImage = hpBar.fillRect.GetComponent<Image>();
+        fillImage.color = photonView.IsMine ? Color.green : Color.red;
+        hpBar.maxValue = health.MaxHP;
+    }
     void Update(){
+        hpBar.maxValue = health.MaxHP;
         hpBar.value = health.HP;
         hpBar.transform.rotation = Quaternion.identity;
         hpBar.transform.position = mover.transform.position + offset;
         nickName.transform.rotation = Quaternion.identity;
         nickName.transform.position = mover.transform.position + offset - new Vector3(0, 0.3f, 0);
+        if(photonView.IsMine) BgHealthBarUI.Instance.SetHPBar(health.MaxHP, health.HP);
     }
 }
