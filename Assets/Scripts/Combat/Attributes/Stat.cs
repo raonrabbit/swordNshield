bf0e4fa7666bcce164db5482abea415b5dcb9fc9
raonrabbit <raonrabbit@gmail.com>
2024-04-26 01:12:07 +0900

    feat: Stat에 원거리 관련 변수 추가

--- Assets/Scripts/Combat/Attributes/Stat.cs
+++ Assets/Scripts/Combat/Attributes/Stat.cs
@@ -1,19 +1,34 @@
+using SwordNShield.Combat.Actions;
 using UnityEngine;
 
 namespace SwordNShield.Combat.Attributes
 {
     public class Stat : MonoBehaviour
     {
+        [Header("Default")]
         [SerializeField] private float maxHP = 200f;
         [SerializeField] private float moveSpeed = 1.5f;
         [SerializeField] private float rotateSpeed = 750f;
+        
+        [Header("Combat")]
+        [SerializeField] private AttackType attackType;
+        [SerializeField] private GameObject bullet;
+        [SerializeField] private float bulletSpeed;
+        [SerializeField] private Vector3 bulletPosition;
         [SerializeField] private float attackRange;
         [SerializeField] private float attackDamage;
         [SerializeField] private float attackCoolTime;
 
+        // Default
         public float HP => maxHP;
         public float MoveSpeed => moveSpeed;
         public float RotateSpeed => rotateSpeed;
+        
+        // Combat
+        public AttackType AttackType => attackType;
+        public GameObject Bullet => bullet;
+        public float BulletSpeed => bulletSpeed;
+        public Vector3 BulletPosition => bulletPosition;
         public float AttackRange => attackRange;
         public float AttackDamage => attackDamage;
         public float AttackCoolTime => attackCoolTime;
